aff043730464855fd3bee542324d5428c15434a3
admin-johan <johan.van.wyk@capgemini.com>
2025-04-05 12:17:25 +0200

    Update app to use MenuBar

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,13 +1,25 @@
 import {Component} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
+import {MenuItem} from "primeng/api";
+import {Avatar} from "primeng/avatar";
+import {Menubar} from "primeng/menubar";
 
 @Component({
     standalone: true,
     selector: 'app-layout',
     templateUrl: './layout.component.html',
     styleUrls: ['./layout.component.scss'],
-    imports: [CommonModule, RouterModule]
+    imports: [CommonModule, RouterModule, Avatar, Menubar]
 })
 export class LayoutComponent {
+    items: MenuItem[] = [];
+
+    ngOnInit() {
+        this.items = [
+            {label: 'Dashboard', icon: 'pi pi-home', routerLink: '/dashboard'},
+            {label: 'Journal', icon: 'pi pi-book', routerLink: '/journal'},
+            // Add more menu items as needed
+        ];
+    }
 }
