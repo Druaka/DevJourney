a377daebcefbc38495b42d2ba5698c628c91e8aa
admin-johan <johan.van.wyk@capgemini.com>
2025-04-11 16:26:51 +0200

    Fetch call to backend from git history

--- frontend/src/app/pages/journal/journal.component.ts
+++ frontend/src/app/pages/journal/journal.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {CardModule} from "primeng/card";
+import {PingService} from '@app/services/ping.service';
 
 @Component({
     standalone: true,
@@ -9,5 +10,21 @@ import {CardModule} from "primeng/card";
     styleUrls: ['./journal.component.scss'],
     imports: [CommonModule, CardModule]
 })
-export class JournalComponent {
+export class JournalComponent implements OnInit {
+    backendMessage: string = 'Loading...';
+
+    constructor(private pingService: PingService) {
+    }
+
+    ngOnInit(): void {
+        this.pingService.ping().subscribe({
+            next: (res) => {
+                this.backendMessage = res.message;
+            },
+            error: (err) => {
+                this.backendMessage = 'Error contacting backend.';
+                console.error(err);
+            }
+        });
+    }
 }
