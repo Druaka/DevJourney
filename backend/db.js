bdea7571e4b08d20b9e0eb9c9b8550d57fefc172
admin-johan <johan.van.wyk@capgemini.com>
2025-05-07 17:11:00 +0200

    Change connection options

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, {
-    tls: true,
-    tlsInsecure: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
