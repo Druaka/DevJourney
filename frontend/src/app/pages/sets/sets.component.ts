2867b4cd90de79f60e97b047844a60ddb270bd05
admin-johan <johan.van.wyk@capgemini.com>
2025-05-12 22:02:02 +0200

    QoL Changes!

--- frontend/src/app/pages/sets/sets.component.ts
+++ frontend/src/app/pages/sets/sets.component.ts
@@ -7,21 +7,30 @@ import {SelectButtonModule} from 'primeng/selectbutton';
 import {FormsModule} from '@angular/forms';
 import {DividerModule} from 'primeng/divider';
 import {TagModule} from "primeng/tag";
+import {DropdownModule} from 'primeng/dropdown';
+import {RippleModule} from 'primeng/ripple';
+import {Button} from "primeng/button";
 
 @Component({
     selector: 'app-sets',
     templateUrl: './sets.component.html',
     styleUrls: ['./sets.component.scss'],
     standalone: true,
-    imports: [CommonModule, Panel, DataViewModule, SelectButtonModule, FormsModule, DividerModule, TagModule]
+    imports: [CommonModule, Panel, DataViewModule, SelectButtonModule, FormsModule, DividerModule, TagModule, DropdownModule, RippleModule, Button]
 })
 export class SetsComponent implements OnInit {
     sets: any[] = [];
     layout: 'grid' | 'list' = 'grid';
     options: { label: string, value: 'grid' | 'list' }[] = [
-        {label: 'Grid', value: 'grid'},
-        {label: 'List', value: 'list'}
+        //{label: 'Grid', value: 'grid'},
+        //{label: 'List', value: 'list'}
     ];
+    sortOptions: { label: string, value: string }[] = [
+        {label: 'Date', value: 'releaseDate'},
+        {label: 'Name', value: 'name'}
+    ];
+    sortKey: string = 'releaseDate';
+    sortOrder: number = -1;
 
     constructor(private pokemonService: PokemonService) {
     }
@@ -29,7 +38,24 @@ export class SetsComponent implements OnInit {
     ngOnInit(): void {
         this.pokemonService.fetchSets().subscribe(data => {
             this.sets = data.data;
-            this.sets.sort((a, b) => new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime());
+            this.sortSets();
+        });
+    }
+
+    setSortOrder(order: number) {
+        this.sortOrder = order;
+        this.sortSets();
+    }
+
+    sortSets() {
+        this.sets.sort((a, b) => {
+            let value1 = a[this.sortKey];
+            let value2 = b[this.sortKey];
+            if (this.sortKey === 'releaseDate') {
+                value1 = new Date(value1).getTime();
+                value2 = new Date(value2).getTime();
+            }
+            return (value1 < value2 ? -1 : (value1 > value2 ? 1 : 0)) * this.sortOrder;
         });
     }
 
