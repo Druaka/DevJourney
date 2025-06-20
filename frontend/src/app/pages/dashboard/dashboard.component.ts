67ce0c7c1a0ab30c2dddd453b36dc89660e37186
druaka <johanvanwyk0@gmail.com>
2025-06-20 18:43:34 +0200

    Add link.component

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,13 +2,14 @@ import {Component} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {CardModule} from "primeng/card";
 import {ButtonModule} from 'primeng/button';
+import {LinkComponent} from "@app/components/link/link.component";
 
 @Component({
     standalone: true,
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
-    imports: [CommonModule, CardModule, ButtonModule]
+    imports: [CommonModule, CardModule, ButtonModule, LinkComponent]
 })
 export class DashboardComponent {
 }
