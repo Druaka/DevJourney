44ebaefb38adf6219fb6353d8d2ed8b39a3784f6
druaka <johanvanwyk0@gmail.com>
2025-06-21 18:49:41 +0200

    Small formatting fixes

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -12,4 +12,11 @@ import {LinkComponent} from "@app/components/link/link.component";
     imports: [CommonModule, CardModule, ButtonModule, LinkComponent]
 })
 export class DashboardComponent {
+    renderUrl: string = "https://render.com";
+    nodejsUrl: string = "https://nodejs.org/";
+    expressjsUrl: string = "https://expressjs.com/";
+    tcgdexUrl: string = "https://tcgdex.dev/";
+    mongodbDiscoveryUrl: string = "https://www.ovhcloud.com/en/lp/mongodb-discovery/";
+    mongodbUrl: string = "http://mongodb.com/";
+    angularUrl: string = "https://angular.dev/";
 }
